0214cb3ab825e2ab65d59d5fc17c03deeb0fdb95
Alexandros K. Langkilde <alexkarambelas@gmail.com>
2017-03-03 13:07:28 +0100

    Machine DSL with variables

--- Model Driven Development/DSL/machine.py
+++ Model Driven Development/DSL/machine.py
@@ -10,10 +10,8 @@ class state():
         self.transition.append(transition)
 
 class transition():
-    start = ""
     end = ""
-    def __init__(self, start, end):
-        self.start = start
+    def __init__(self, end):
         self.end = end
 
 class variable():
@@ -57,8 +55,8 @@ class machine():
         self.current_count += 1
         return self
 
-    def transition(self, start, end):
-        newTransition = transition(start,end)
+    def transition(self, end):
+        newTransition = transition(end)
         self.list_state[self.current_count].setTransition(newTransition)
         return self
 
@@ -73,8 +71,10 @@ class machine():
             for k in self.list_var:
                 if execute_list[i].lower() == k.name.lower():
                     if execute_list[i+1].lower() == "up":
+                        print("Variable " + k.name + " changed from " + str(k.value) + " to " + str(k.value + 1))
                         k.increase()
                     elif execute_list[i+1].lower() == "down":
+                        print("Variable " + k.name + " changed from " + str(k.value) + " to " + str(k.value - 1))
                         k.decrease()
 
                     i += 1
@@ -85,23 +85,13 @@ class machine():
     def changestate(self, new_state):
         for i in self.state.transition:
             if i.end.lower() == new_state.name.lower():
-                print("Transition from " + i.start + " to " + new_state.name)
+                print("Transition from " + self.state.name + " to " + new_state.name)
                 self.state = new_state
 
     def variable(self,name, min,max):
         newVar = variable(name,min,max)
         self.list_var.append(newVar)
         return self
-
-    def increment(self, var):
-        for i in self.list_var:
-            if i.name == var.name:
-                i.increase()
-
-    def decrement(self, var):
-        for i in self.list_var:
-            if i.name == var.name:
-                i.decrease()
 
     def print(self):
         print(self.name)
@@ -111,7 +101,7 @@ class machine():
         for i in self.list_state:
             print("State: " + i.name)
             for j in i.transition:
-                print("Transition: " +j.start + " " + j.end)
+                print("Transition: " + i.name + " " + j.end)
 
 
 
@@ -122,9 +112,11 @@ TV = machine()
         variable("Volume",0,100).
         variable("Channel",0,100).
             state_add("ON").
-                transition("ON","OFF").
+                transition("OFF").
             state_add("OFF").
-                transition("OFF", "ON")
+                transition("ON").
+                transition("NONE").
+            state_add("NONE")
 
 }
 
@@ -133,10 +125,12 @@ print(TV.name)
 
 print("")
 
+#TV.execute(["ON", "OFF", "OFF", "OFF", "NONE"])
+
 TV.execute(["ON", "OFF", "ON","Channel", "UP","Volume", "UP", "Volume", "UP", "Volume", "DOWN", "NONE", "OFF", ])
 
 print("")
 
-TV.print()
+
 
 
